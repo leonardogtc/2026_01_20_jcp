e8d87877e6e342ab818743fefa6ca4ee8bee82af
Leonardo G.T. da Conceicao <leonardogtc@gmail.com>
2026-01-21 00:43:07 -0300

    Inserindo saldo e depósito nas classes

--- src/com/leonardogtc/jcp/cap003/Conta.java
+++ src/com/leonardogtc/jcp/cap003/Conta.java
@@ -3,10 +3,31 @@ package com.leonardogtc.jcp.cap003;
 public class Conta {
     // Variável de instância
     private String nome;
+    private double saldo;
 
     // Construtor da classe
-    public Conta(String nome) {
+    public Conta(String nome, double saldo) {
         this.nome = nome;
+
+        /*
+        Validar que o saldo é maior que zero (0.0); caso não seja, a variável
+        de instância saldo mantêm seu valor inicial como 0.0.
+         */
+        if (saldo > 0.0) {
+            this.saldo = saldo;
+        }
+    }
+
+    // Depósito
+    public void deposito(double valorDeposito) {
+        if(valorDeposito > 0.0) {
+            saldo += valorDeposito;
+        }
+    }
+
+    // método retorna o saldo da conta
+    public double getSaldo() {
+        return saldo;
     }
 
     // Método para definir o nome no objeto
--- src/com/leonardogtc/jcp/cap003/ContaTeste.java
+++ src/com/leonardogtc/jcp/cap003/ContaTeste.java
@@ -1,15 +1,34 @@
 package com.leonardogtc.jcp.cap003;
 
+import java.util.Scanner;
+
 public class ContaTeste {
     static void main(String[] args) {
 
         // Cria um objeto conta e o atribui a minhaConta
-        Conta conta1 = new Conta("Jane Green");
-        Conta conta2 = new Conta("John Blue");
+        Conta conta1 = new Conta("Jane Green", 50.00);
+        Conta conta2 = new Conta("John Blue", -7.53);
+
+        // Exibe o saldo inicial
+        System.out.printf("Saldo %s: $%.2f%n", conta1.getNome(), conta1.getSaldo());
+        System.out.printf("Saldo %s: $%.2f%n", conta2.getNome(), conta2.getSaldo());
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Entre com valor de depósito para conta 1: ");
+        double valorDeposito = input.nextDouble();
+        System.out.printf("Depositando $%.2f", valorDeposito);
+        conta1.deposito(valorDeposito);
+
+        System.out.print("Entre com valor de depósito para conta 2: ");
+        valorDeposito = input.nextDouble();
+        System.out.printf("Depositando $%.2f", valorDeposito);
+        conta2.deposito(valorDeposito);
 
         // Exibir o nome armazenado no objeto minhaConta
-        System.out.printf("O nome do objeto conta1 é: %s%n", conta1.getNome());
-        System.out.printf("O nome do objeto conta2 é: %s%n", conta2.getNome());
+        System.out.printf("O nome do objeto conta1 é: %s e seu saldo é $%.2f%n", conta1.getNome(), conta1.getSaldo());
+        System.out.printf("O nome do objeto conta1 é: %s e seu saldo é $%.2f%n", conta2.getNome(), conta2.getSaldo());
+
+        input.close();
 
     }
 }
