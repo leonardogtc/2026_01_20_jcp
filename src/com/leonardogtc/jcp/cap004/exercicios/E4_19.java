525bd34e2f9ffb71c73d75bb8287a410c4cc64c7
Leonardo G.T. da Conceicao <leonardogtc@gmail.com>
2026-01-24 09:47:55 -0300

    Implementação de melhorias no exercício E4_19 - Acrescentando String para listagem dos itens e valores

--- src/com/leonardogtc/jcp/cap004/exercicios/E4_19.java
+++ src/com/leonardogtc/jcp/cap004/exercicios/E4_19.java
@@ -11,13 +11,16 @@ public class E4_19 {
         double valor = 0.0;
         double totalValor = 0.0;
         double comissao = 0.0;
+        StringBuilder listagem = new StringBuilder("Item\tValor\n");
 
         // Entrada de dados
         System.out.print("Digite o valor do item pi -1 para encerra o programa: ");
         valor = input.nextDouble();
 
+
         if(valor != -1) {
             totalValor += valor;
+            listagem.append(String.format("%d\t\t%.2f%n", item, valor));
             item++;
         }
 
@@ -27,12 +30,15 @@ public class E4_19 {
 
             if(valor != -1) {
                 totalValor += valor;
+                listagem.append(String.format("%d\t\t%.2f%n", item, valor));
                 item++;
             }
         }
 
         // Mostra saída de dados
         if(totalValor != 0) {
+
+            System.out.println(listagem);
             comissao = 200 + (totalValor * 0.09);
 
             System.out.printf("O total de itens é: %d%n", item);
--- src/com/leonardogtc/jcp/cap005/WhileCounter.java
+++ src/com/leonardogtc/jcp/cap005/WhileCounter.java
@@ -1,4 +1,13 @@
 package com.leonardogtc.jcp.cap005;
 
 public class WhileCounter {
+    static void main(String[] args) {
+        int contador = 1;
+
+        while(contador <= 10) {
+            System.out.printf("%d ", contador);
+            ++contador;
+        }
+        System.out.println();
+    }
 }
