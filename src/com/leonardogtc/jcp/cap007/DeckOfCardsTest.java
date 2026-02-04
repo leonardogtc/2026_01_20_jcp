18ae2eb978c93e6eed58866742b0a92d4c83bcf8
Leonardo G.T. da Conceicao <leonardogtc@gmail.com>
2026-02-04 17:28:03 -0300

    DeckOfCards - Estudo de Métodos e Arrays

---
+++ src/com/leonardogtc/jcp/cap007/DeckOfCardsTest.java
@@ -0,0 +1,19 @@
+package com.leonardogtc.jcp.cap007;
+
+public class DeckOfCardsTest {
+    public static void main(String[] args) {
+        DeckOfCards myDeckOfCards = new DeckOfCards();
+        myDeckOfCards.shuffle(); // coloca Card em ordem aleatória
+
+        // imprime todas as 52 cartas na ordem em que elas são distribuídas
+        for (int i = 1; i <= 52; i++) {
+            // distribui e exibe uma Carta
+            System.out.printf("%-19s", myDeckOfCards.dealCard());
+
+            if (i % 4 == 0) { // gera uma nova linha após cada quarta carta
+                System.out.println();
+            }
+        }
+    }
+
+}
